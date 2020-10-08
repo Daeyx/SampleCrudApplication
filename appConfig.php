23739888e390bd6e7045eea12d338dbb4644334b
Garth Robertson <garthwrobertson@gmail.com>
2020-10-07 21:09:34 -0500

    testing a commit

--- appConfig.php
+++ appConfig.php
@@ -1,7 +1,7 @@
 <?php
 
 // This is clearly for demo purposes.  Never store secrets in plain text in production or any 
-// environment for that matter.  Use a secrets managment tool.
+// environment for that matter.  Use a secrets managment tool.    
 return array(
     'host' => 'localhost',
     'username' => 'root',
